a0de163030b016a221552ddfb26ed7fd6e800c21
Simon Sawicki <37424085+Grub4K@users.noreply.github.com>
2022-01-18 19:00:01 +0100

    Remove usage of SMU

--- Patches/SkipIntroPatch.cs
+++ Patches/SkipIntroPatch.cs
@@ -1,17 +1,17 @@
 ﻿using HarmonyLib;
 using UnityEngine;
-using SMU.Reflection;
+
 namespace NoIntro
 {
     public static class NoIntroPatches
     {
         [HarmonyPatch(typeof(StartupScene), "Update")]
         [HarmonyPrefix]
-        private static void StartupScene_Update_Prefix(StartupScene __instance)
+        private static void StartupScene_Update_Prefix(StartupScene __instance,
+            GameObject ___startupAnimationObject)
         {
             __instance.animationLength = 0f;
-            __instance.GetField<StartupScene, GameObject>("startupAnimationObject").SetActive(false);
+            ___startupAnimationObject.SetActive(false);
         }
-
-    } 
+    }
 }

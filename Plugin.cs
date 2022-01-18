352196a1754cfcbb34d7df53be49cccb16e1a4f7
Pink <62712899+BSMGPink@users.noreply.github.com>
2022-01-18 00:57:54 +0000

    ModdyWoddy

---
+++ Plugin.cs
@@ -0,0 +1,39 @@
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using SMU.Reflection;
+namespace NoIntro
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    class Plugin : BaseUnityPlugin
+    {
+        internal static Plugin Instance;
+
+        void Awake()
+        {
+            if (Instance)
+            {
+                DestroyImmediate(this);
+                return;
+            }
+            Instance = this;
+
+            // Init logs and patches.
+            var harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+
+            harmony.PatchAll(typeof(NoIntroPatches));
+
+        }
+        #region logging
+        internal static void LogDebug(string message) => Instance.Log(message, LogLevel.Debug);
+        internal static void LogInfo(string message) => Instance.Log(message, LogLevel.Info);
+        internal static void LogWarning(string message) => Instance.Log(message, LogLevel.Warning);
+        internal static void LogError(string message) => Instance.Log(message, LogLevel.Error);
+        private void Log(string message, LogLevel logLevel) => Logger.Log(logLevel, message);
+        #endregion
+    }
+}
+
---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿
+namespace NoIntro
+{
+    class PluginInfo
+    {
+        public const string PLUGIN_GUID = "com.pink.spin.nointro";
+        public const string PLUGIN_NAME = "No Intro";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
